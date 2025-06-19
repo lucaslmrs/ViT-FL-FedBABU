e68290430ed6e45c1cd5d2824ca5cb1d106177bc
lucas <rlucasmatheus@gmail.com>
2025-06-18 23:49:02 -0300

    VIT-FL running both in cpu/gpu

--- FedBABU-master/scripts/run_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python main_local.py --dataset cifar100 --model mobile --num_classes 100 --shard_per_user 10 --epochs 160 --lr 0.1 --num_users 100 --results_save local
-python main_local.py --dataset cifar100 --model mobile --num_classes 100 --shard_per_user 50 --epochs 160 --lr 0.1 --num_users 100 --results_save local
-python main_local.py --dataset cifar100 --model mobile --num_classes 100 --shard_per_user 100 --epochs 160 --lr 0.1 --num_users 100 --results_save local
